281f6896aa07c33e159c91e141058689b5cc2677
Toddeh Alexander <alexandertoddeh@gmail.com>
2023-09-07 16:02:18 -0700

    update

---
+++ script.js
@@ -0,0 +1,10 @@
+function openNotepad() {
+    // Add code to open Notepad or perform a specific action
+    alert("Opening Notepad...");
+}
+
+function openCalculator() { 
+    // Add code to open Calculator or perform a specific action
+    alert("Opening Calculator...");
+}
+// Add more functions for other icons as needed
