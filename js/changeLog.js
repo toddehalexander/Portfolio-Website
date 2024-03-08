3a0d877f073538aa876042e0dea3f1044ab7e0f2
Toddeh <alexandertoddeh@gmail.com>
2024-03-08 01:47:36 -0800

    updated formating, fixed all issues as of now.

--- js/changeLog.js
+++ js/changeLog.js
@@ -1,13 +1,21 @@
 function toggleContent(event) {
     event.stopPropagation();
     var target = event.target;
-    if (target.classList.contains('date') || target.parentElement.classList.contains('date')) {
+    if (target && (target.classList.contains('date') || (target.parentElement && target.parentElement.classList.contains('date')))) {
         var content = target.classList.contains('date') ? target.nextElementSibling : target.parentElement.nextElementSibling;
-        if (content.classList.contains('dropdown-content')) {
+        if (content && content.classList.contains('dropdown-content')) {
             if (content.style.maxHeight) {
                 content.style.maxHeight = null;
+                // If the content is being closed, store its state in sessionStorage
+                if (content.id === 'newest') {
+                    sessionStorage.setItem('aboutMeClosed', 'true');
+                }
             } else {
                 content.style.maxHeight = content.scrollHeight + "px";
+                // If the content is being opened, remove its state from sessionStorage
+                if (content.id === 'newest') {
+                    sessionStorage.removeItem('aboutMeClosed');
+                }
             }
         }
     }
@@ -17,16 +25,13 @@ document.addEventListener('click', toggleContent);
 
 window.onload = function() {
     var firstContent = document.getElementById('newest');
-    firstContent.style.maxHeight = firstContent.scrollHeight + "px";
-    // Add this line to make the text inside not selectable
-    firstContent.style.userSelect = "none";
-};
-
-document.addEventListener('click', toggleContent);
-
-
-// Open the first dropdown content when the page is loaded
-window.onload = function() {
-    var firstContent = document.getElementById('newest');
-    firstContent.style.maxHeight = firstContent.scrollHeight + "px";
+    if (firstContent) {
+        // Check if the about me section was previously closed
+        if (sessionStorage.getItem('aboutMeClosed') === 'true') {
+            firstContent.style.maxHeight = null;
+        } else {
+            firstContent.style.maxHeight = firstContent.scrollHeight + "px";
+        }
+        firstContent.style.userSelect = "none";
+    }
 };
