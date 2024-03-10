aaa30fc6ebb684dfb71fe1df4f5172e1ae44c453
Toddeh <alexandertoddeh@gmail.com>
2024-03-10 14:51:45 -0700

    added animation for name bouncing up and down

---
+++ js/name.js
@@ -0,0 +1,21 @@
+const name = document.getElementById('name');
+const [firstName, lastName] = 'Toddeh Alexander'.split(' ');
+name.innerHTML = `
+  <div class="name-part">${firstName.split('').map((letter, i) => `<span class="letter drop" style="--delay:${i * 0.1}s">${letter}</span>`).join('')}</div>
+  <div class="name-part">${lastName.split('').map((letter, i) => `<span class="letter drop" style="--delay:${i * 0.1}s">${letter}</span>`).join('')}</div>
+`;
+
+// After the initial drop, start the bounce animation for a random letter every 2 seconds
+setTimeout(() => {
+  const letters = document.querySelectorAll('.letter');
+  letters.forEach(letter => {
+    letter.classList.remove('drop');
+  });
+  setInterval(() => {
+    const randomLetter = letters[Math.floor(Math.random() * letters.length)];
+    randomLetter.classList.add('bounce');
+    setTimeout(() => {
+      randomLetter.classList.remove('bounce');
+    }, 400);
+  }, 2000);
+}, (firstName.length + lastName.length) * 100 + 1000); // Adjust this value as needed
