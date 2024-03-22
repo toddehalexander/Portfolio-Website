0ba14ec733434fe576b8816dc372ffa5d8983d8e
Toddeh <alexandertoddeh@gmail.com>
2024-03-22 02:57:24 -0700

    update changelog css and js

--- js/changeLog.js
+++ js/changeLog.js
@@ -7,34 +7,11 @@ function toggleContent(event) {
             // Check if the dropdown is already open
             if (content.style.maxHeight) {
                 content.style.maxHeight = null;
-                // If the content is being closed, store its state in localStorage
-                if (content.id === 'newest') {
-                    localStorage.setItem('dropDownClosed', 'true');
-                }
             } else {
                 content.style.maxHeight = content.scrollHeight + "px";
-                // If the content is being opened, remove its state from localStorage
-                if (content.id === 'newest') {
-                    localStorage.removeItem('dropDownClosed');
-                }
             }
         }
     }
 }
 
 document.addEventListener('click', toggleContent);
-
-window.onload = function() {
-    var firstContent = document.getElementById('newest');
-    if (firstContent) {
-        // Check localStorage for 'dropDownClosed' item
-        var dropDownClosed = localStorage.getItem('dropDownClosed');
-        // If 'dropDownClosed' is not 'true', open the about me section on window load
-        if (dropDownClosed !== 'true') {
-            firstContent.style.maxHeight = firstContent.scrollHeight + "px";
-        } else {
-            // If 'dropDownClosed' is 'true', keep the about me section closed
-            firstContent.style.maxHeight = null;
-        }
-    }
-};
