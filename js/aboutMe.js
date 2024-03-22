11220cc12b70667f00fb57ae7634bdab7a4f57de
Toddeh Alexander <alexandertoddeh@gmail.com>
2024-03-21 18:40:34 -0700

    removed pagefade css & js, added seperarte local storage to aboutme, changelog

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -9,13 +9,13 @@ function toggleContent(event) {
                 content.style.maxHeight = null;
                 // If the content is being closed, store its state in localStorage
                 if (content.id === 'newest') {
-                    localStorage.setItem('dropDownClosed', 'true');
+                    localStorage.setItem('aboutMeClosed', 'true');
                 }
             } else {
                 content.style.maxHeight = content.scrollHeight + "px";
                 // If the content is being opened, remove its state from localStorage
                 if (content.id === 'newest') {
-                    localStorage.removeItem('dropDownClosed');
+                    localStorage.removeItem('aboutMeClosed');
                 }
             }
         }
@@ -27,13 +27,13 @@ document.addEventListener('click', toggleContent);
 window.onload = function() {
     var firstContent = document.getElementById('newest');
     if (firstContent) {
-        // Check localStorage for 'dropDownClosed' item
-        var dropDownClosed = localStorage.getItem('dropDownClosed');
-        // If 'dropDownClosed' is not 'true', open the about me section on window load
-        if (dropDownClosed !== 'true') {
+        // Check localStorage for 'aboutMeClosed' item
+        var aboutMeClosed = localStorage.getItem('aboutMeClosed');
+        // If 'aboutMeClosed' is not 'true', open the about me section on window load
+        if (aboutMeClosed !== 'true') {
             firstContent.style.maxHeight = firstContent.scrollHeight + "px";
         } else {
-            // If 'dropDownClosed' is 'true', keep the about me section closed
+            // If 'aboutMeClosed' is 'true', keep the about me section closed
             firstContent.style.maxHeight = null;
         }
     }
